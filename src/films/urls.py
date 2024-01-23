3379232a5d36135593ed4189070c127aea2e1351
noahwekesa <noahwekesa0@gmail.com>
2024-01-23 22:45:51 +0300

    fix authentications

--- src/films/urls.py
+++ src/films/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path("register/", views.RegisterView.as_view(), name="register"),
-    path("film/", views.AddFilms, name='add-film'),
+    path("film/", views.FilmList.as_view(), name='film-list'),
 ]
 
 htmx_urlpatterns = [
     path('check_username/', views.check_username, name='check-username'),
+    path('add-film', views.add_film, name='add-film'),
+    path('delete-film/<int:pk>/', views.delete_film, name='delete-film'),
 ]
 urlpatterns += htmx_urlpatterns
--- src/films/views.py
+++ src/films/views.py
@@ -1,14 +1,15 @@
-from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, login_required
 from django.urls import reverse_lazy
-from django.views.generic import FormView, TemplateView
+from django.views.generic import FormView, ListView, TemplateView
 from django.contrib.auth import get_user_model, logout
+from .models import Film
+
 
 from .forms import RegisterForm
-
-# Create your views here.
 
 
 class IndexView(TemplateView):
@@ -42,6 +43,41 @@ def check_username(request):
         return HttpResponse("<div id='username-error' class='text-success'>This username is available </div>")
 
 
+class FilmList(LoginRequiredMixin, ListView):
+    template_name = 'AddFilms.html'
+    model = Film
+    context_object_name = 'films'
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.films.all()
+
+
+def add_film(request):
+    name = request.POST.get('filmname')
+    # create an isinstance of the film
+    film = Film.objects.create(name=name)
+    # add the film to the users list
+    request.user.films.add(film)
+    # return template with all users films
+    films = request.user.films.all()
+    context = {
+        'films': films
+    }
+    return render(request, 'partials/film-list.html', context)
+
+
 @login_required
-def AddFilms(request):
-    return render(request, 'AddFilms.html')
+@require_http_methods(["DELETE"])
+def delete_film(request, pk):
+    """
+function to delete film from user's list
+    """
+    request.user.films.remove(pk)
+    # return the template
+    films = request.user.films.all()
+
+    context = {
+        'films': films
+    }
+    return render(request, 'partials/film-list.html', context)
