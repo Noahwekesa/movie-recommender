ce7e270cd2dc7e24357f6ddeeae5c2e29603d315
noahwekesa <noahwekesa0@gmail.com>
2024-03-13 17:18:10 +0300

    fixed pycodestylee

--- src/films/views.py
+++ src/films/views.py
@@ -13,17 +13,17 @@ from .forms import RegisterForm
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = "index.html"
 
 
 class Login(LoginView):
-    template_name = 'registration/login.html'
+    template_name = "registration/login.html"
 
 
 class RegisterView(FormView):
     form_class = RegisterForm
-    template_name = 'registration/register.html'
-    success_url = reverse_lazy('login')
+    template_name = "registration/register.html"
+    success_url = reverse_lazy("login")
 
     def form_valid(self, form):
         form.save()  # save the user
@@ -32,52 +32,53 @@ class RegisterView(FormView):
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
 
 
 def check_username(request):
-    username = request.POST.get('username')
+    username = request.POST.get("username")
     if get_user_model().objects.filter(username=username).exists():
-        return HttpResponse("<div id='username-error' class='text-danger'>This username already exists</div>")
+        return HttpResponse(
+            "<div id='username-error' class='text-danger'>This username already exists</div>"
+        )
     else:
-        return HttpResponse("<div id='username-error' class='text-success'>This username is available </div>")
+        return HttpResponse(
+            "<div id='username-error' class='text-success'>This username is available </div>"
+        )
 
 
 class FilmList(LoginRequiredMixin, ListView):
-    template_name = 'AddFilms.html'
+    template_name = "AddFilms.html"
     model = Film
-    context_object_name = 'films'
+    context_object_name = "films"
 
     def get_queryset(self):
         user = self.request.user
         return user.films.all()
 
 
+@login_required
 def add_film(request):
-    name = request.POST.get('filmname')
+    name = request.POST.get("filmname")
     # create an isinstance of the film
     film = Film.objects.create(name=name)
     # add the film to the users list
     request.user.films.add(film)
     # return template with all users films
     films = request.user.films.all()
-    context = {
-        'films': films
-    }
-    return render(request, 'partials/film-list.html', context)
+    context = {"films": films}
+    return render(request, "partials/film-list.html", context)
 
 
 @login_required
 @require_http_methods(["DELETE"])
 def delete_film(request, pk):
     """
-function to delete film from user's list
+    function to delete film from user's list
     """
     request.user.films.remove(pk)
     # return the template
     films = request.user.films.all()
 
-    context = {
-        'films': films
-    }
-    return render(request, 'partials/film-list.html', context)
+    context = {"films": films}
+    return render(request, "partials/film-list.html", context)
